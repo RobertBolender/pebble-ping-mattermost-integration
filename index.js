6f92a2d7c8f0d080a4866815d9a21b00aa8f855b
Bob Bolender <bob.bolender@fahrenheit.io>
2016-06-22 15:39:24 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express'),
+    app = express(),
+    request = require('request');
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.post('/score/:winner/:winnerScore/:loser/:loserScore', function (req, res) {
+  res.send('Congratulations ' + req.params.winner + '!\n');
+  var data = 'payload={"text": "| Player | Score |\\n|:-------|:--------|\\n| ' +
+    req.params.winner + ' | ' + req.params.winnerScore  + ' :fireworks: |\\n| ' +
+    req.params.loser  + ' | ' + req.params.loserScore   + ' |"}';
+  request.post(process.env.WEBHOOK_URL).form(data);
+});
+
+app.listen(3003, function(){
+  console.log('App listening on port 3003!');
+});
