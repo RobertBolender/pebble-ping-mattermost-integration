0ec1e75c054922505d2e70300528dd477bfab5cf
Bob Bolender <bob.bolender@fahrenheit.io>
2016-06-28 08:21:27 -0500

    Use PingPongBot

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ app.get('/', function (req, res) {
 
 app.post('/score/:winner/:winnerScore/:loser/:loserScore', function (req, res) {
   res.send('Congratulations ' + req.params.winner + '!\n');
-  var data = 'payload={"text": "| Player | Score |\\n|:-------|:--------|\\n| ' +
+  var data = 'payload={"username": "PingPongBot",' +
+    '"text": "| Player | Score |\\n|:-------|:--------|\\n| ' +
     req.params.winner + ' | ' + req.params.winnerScore  + ' :fireworks: |\\n| ' +
     req.params.loser  + ' | ' + req.params.loserScore   + ' |"}';
   request.post(process.env.WEBHOOK_URL).form(data);
